c2427ee73330e40ba1163d182739f27856f19870
Mahmoud Ahamed Elsaramji <90051301+elsaramji@users.noreply.github.com>
2025-07-28 03:27:11 +0300

    refactoring test features

--- test/features/personal/data/models/personal_model_test.dart
+++ test/features/personal/data/models/personal_model_test.dart
@@ -6,10 +6,10 @@ import 'package:portfoilosr/features/personal/data/modles/personal_model.dart';
 import 'package:portfoilosr/features/personal/domain/entities/personal_entity.dart';
 
 void main() {
-  group("test PersonalModel", () {
-    test("test model as PersonalEntity ", () {
-      // assert
-      PersonalModel model = PersonalModel(
+  group("test result of data as PersonalModel and PersonalEntity", () {
+    test("test model result as PersonalEntity ", () {
+      // act
+      PersonalModel result = PersonalModel(
         firstName: '',
         lastName: '',
         headLine: '',
@@ -20,19 +20,19 @@ void main() {
         about: '',
         socialLinks: {},
       );
-      // act
-      expect(model, isA<PersonalEntity>());
+      // assert
+      expect(result, isA<PersonalEntity>());
     });
 
-    test("test model from json", () {
+    test("test result from json as PersonalModel and PersonalEntity", () {
       // arrange
       final json = fixture('personal_data');
       final personal = jsonDecode(json);
-      // assert
-      PersonalModel model = PersonalModel.fromJson(personal);
       // act
-      expect(model, isA<PersonalModel>());
-      expect(model, isA<PersonalEntity>());
+      PersonalModel result = PersonalModel.fromJson(personal);
+      // assert
+      expect(result, isA<PersonalModel>());
+      expect(result, isA<PersonalEntity>());
     });
   });
 }
